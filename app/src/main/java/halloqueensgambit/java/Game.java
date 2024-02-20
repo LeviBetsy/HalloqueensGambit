52825b30456e35561fd3398b2d23dcbb1ba03d69
aidenseibel <aiden.seibel@gmail.com>
2024-02-19 18:57:13 -0600

    Added zobrist hashing (needs fix)

--- app/src/main/java/halloqueensgambit/java/Game.java
+++ app/src/main/java/halloqueensgambit/java/Game.java
@@ -13,6 +13,8 @@ public class Game{
     private Pos wKing;
     private Pos bKing;
     private int evaluation;
+    private long zobristHash;
+    private long[] zobristNumbers;
     static char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
 
     /*                           SETTERS, GETTERS, CONSTRUCTORS                           */
@@ -33,12 +35,22 @@ public class Game{
         return (this.side == Side.WHITE) ? bKing : wKing;
     }
 
+    public Game(){
+        this("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    }
+
     public Game(Side side, Board board){
         this.side = side;
         this.board = board;
         this.evaluation = board.evaluate();
         this.wKing = board.findKing(Side.WHITE);
         this.bKing = board.findKing(Side.BLACK);
+        this.zobristNumbers = new long[769];
+        for(int i = 0; i < 769; i++){
+            zobristNumbers[i] = (long) (Math.random() * Math.pow(2, 64) - 1);
+        }
+        generateZobristHash();
+
     }
 
     // rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
@@ -56,6 +68,12 @@ public class Game{
         this.evaluation = board.evaluate();
         this.wKing = board.findKing(Side.WHITE);
         this.bKing = board.findKing(Side.BLACK);
+        this.zobristNumbers = new long[769];
+
+        for(int i = 0; i < 769; i++){
+            zobristNumbers[i] = (long) (Math.random() * Math.pow(2, 64) - 1);
+        }
+        generateZobristHash();
     }
     
 
@@ -169,8 +187,55 @@ public class Game{
         return moves;
     }
 
-    public Game copy(){
-        return new Game(this.side, this.board);
+    private int getZobristIndex(Pos pos, Piece piece){
+        int index = (pos.x-1) + (pos.y-1) * 8;
+        
+        if(piece instanceof Pawn){
+            index = 0;
+        }else if(piece instanceof Knight){
+            index = 1;
+        }else if(piece instanceof Bishop){
+            index = 2;
+        }else if(piece instanceof Rook){
+            index = 3;
+        }else if(piece instanceof Queen){
+            index = 4;
+        }else{
+            index = 5;
+        }
+        if(piece.side() == Side.BLACK){
+            index += 6;
+        }
+        return index;
+    }
+
+    public long getZobristHash(){
+        return this.zobristHash;
+    }
+
+    public long generateZobristHash(){
+        // 781 (12*64 + 1 + 4 + 8)
+        // TODO: en passant and castling rights for zobrist numbers. 
+        // for now they are left out. this will cause bugs in certain positions involving castling/en passant.
+        long hash = 0;
+        int sideIsBlack = 768;
+
+        for(Map.Entry<Pos, Piece> entry: this.board){
+            Pos pos = entry.getKey();
+            Piece piece = entry.getValue();
+
+            //pawn, knight, bishop, rook, queen, king * 2
+            int index = getZobristIndex(pos, piece);
+
+            hash = hash ^ zobristNumbers[index];
+        }
+
+        if(this.side == Side.BLACK){
+            hash = hash ^ zobristNumbers[sideIsBlack];
+        }
+
+        this.zobristHash = hash;
+        return hash;
     }
 
     @Override
@@ -183,8 +248,6 @@ public class Game{
 
     @Override
     public boolean equals(Object obj){
-
-
         if(!(obj instanceof Game)) return false;
         Game other = (Game) obj;
         return this.side.equals(other.side) && this.board.equals(other.board);
@@ -194,7 +257,10 @@ public class Game{
         Optional<Piece> captured = this.board.lookup(move.end);
 
         this.side = Side.opponent(side);
+        zobristHash = zobristHash ^ this.zobristNumbers[768];
+
         Piece movingPiece = this.board.data.remove(move.start);
+        zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(move.start, movingPiece)];
 
         if (movingPiece instanceof King) {
             if (movingPiece.side() == Side.WHITE)
@@ -217,11 +283,11 @@ public class Game{
             if (move.end.y() == 1 || move.end.y() == 8){
                 Queen newQueen = new Queen(movingPiece.side());
                 this.board.data.put(move.end, newQueen);
+                zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(move.end, newQueen)];
+
                 //handling evaluation
                 this.evaluation -= movingPiece.value();
                 this.evaluation += newQueen.value();
-            } else {
-                this.board.data.put(move.end, movingPiece);
             }
         } 
 
@@ -232,22 +298,23 @@ public class Game{
             if (move.start.equals(new Pos(5,castleY))
                     && move.end.equals(new Pos(3,castleY))){
                 //MOVING THE ROOK
-                this.board.data.remove(new Pos(1,castleY));
+                Piece movingRook = this.board.data.remove(new Pos(1,castleY));
+                zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(1,castleY), movingRook)];
                 this.board.data.put(new Pos(4,castleY), new Rook(movingPiece.side(), true));
-                this.board.data.put(move.end, movingPiece);
+                zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(4,castleY), movingRook)];
             //CASTLING KING SIDE
             } else if (move.start.equals(new Pos(5, castleY))
                     && move.end.equals(new Pos(7, castleY))){
                 //MOVING THE ROOK
-                this.board.data.remove(new Pos(8,castleY));
+                Piece movingRook = this.board.data.remove(new Pos(8,castleY));
+                zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(8,castleY), movingRook)];
                 this.board.data.put(new Pos(6,castleY), new Rook(movingPiece.side(), true));
-                this.board.data.put(move.end, movingPiece);
-            } else {
-                this.board.data.put(move.end, movingPiece);
+                zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(6,castleY), movingRook)];
             }
-        } else {
-            this.board.data.put(move.end, movingPiece);
-        }
+        } 
+
+        this.board.data.put(move.end, movingPiece);
+        zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(move.end, movingPiece)];
 
         captured.ifPresent(piece -> this.evaluation -= piece.value());
         return captured;
@@ -255,7 +322,10 @@ public class Game{
 
     public void unMakeMove(Move move, Optional<Piece> captured){
         this.side = Side.opponent(side);
+        zobristHash = zobristHash ^ this.zobristNumbers[768];
+
         Piece movingPiece = this.board.data.remove(move.end);
+        zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(move.end, movingPiece)];
 
         if (movingPiece instanceof King){
             if (movingPiece.side() == Side.WHITE)
@@ -264,19 +334,22 @@ public class Game{
                 this.bKing = move.start;
         }
         
-        // handle captures AND promotions
+        // handle captures
         if(captured.isPresent()) {
             this.board.data.put(move.end, captured.get());
+            zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(move.end, captured.get())];
             this.evaluation += captured.get().value();
         }
 
         // put the moving piece back where it goes
         this.board.data.put(move.start, movingPiece);
+        zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(move.start, movingPiece)];
 
-        //PROMOTION
+        //PROMOTION 
         if(move.isPromotion){
             Pawn pawn = new Pawn(this.side);
             this.board.data.put(move.start, pawn);
+            zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(move.start, pawn)];
             //handling evaluation
             this.evaluation += pawn.value();
             this.evaluation -= movingPiece.value();
@@ -291,14 +364,19 @@ public class Game{
                 // put the king rook back
                 if(move.end.x() == 7){
                     Rook rook = (Rook) this.board.data.remove(new Pos(6, castleY));
+                    zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(6, castleY), rook)];
                     rook.hasMoved = false;
                     this.board.data.put(new Pos(8, castleY), rook);
+                    zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(8, castleY), rook)];
+
                 }
-                // put the queen rook back
+                // put the queen rook back 
                 else if(move.end.x() == 3){
                     Rook rook = (Rook) this.board.data.remove(new Pos(4, castleY));
+                    zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(4, castleY), rook)];
                     rook.hasMoved = false;
                     this.board.data.put(new Pos(1, castleY), rook);
+                    zobristHash = zobristHash ^ this.zobristNumbers[getZobristIndex(new Pos(1, castleY), rook)];
                 }
             }
         }
--- app/src/test/java/halloqueensgambit/java/SolverTesting.java
+++ app/src/test/java/halloqueensgambit/java/SolverTesting.java
@@ -5,11 +5,50 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+
 import java.util.Scanner;
+import java.util.List;
+import java.util.Optional;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import halloqueensgambit.java.piece.*;
 
 import org.junit.jupiter.api.Test;
 
 public class SolverTesting {
+    // ensures transposition table hashing is working correctly
+    @Test void testZobristHashingBig(){
+        Game game = new Game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        long zobrist = game.getZobristHash();
+        Solver solver = new Solver(game, 5);
+        solver.alphaBetaSearch(5, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        assertEquals(zobrist, game.getZobristHash());
+    }
+
+    @Test void testZobristHashingSmall(){
+        Game game = new Game("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        long zobrist = game.getZobristHash();
+        for(int i = 1; i < 4; i ++){
+            Solver solver = new Solver(game, i);
+            System.out.println(i);
+            solver.alphaBetaSearch(i, Integer.MIN_VALUE, Integer.MAX_VALUE);
+            assertEquals(zobrist, game.getZobristHash());
+        }
+
+        List<Move> allLegalMoves = game.getLegalMoves();
+        for(Move m: allLegalMoves){
+            Optional<Piece> captured = game.makeMove(m);
+            System.out.println(m);
+            game.unMakeMove(m, captured);
+            assertEquals(zobrist, game.getZobristHash());
+        }
+    }
+
     // all straightforward checkmates. if fails, something is definitely wrong
     @Test void testCheckMateInOne(){
         File puzzles = new File("./src/test/java/halloqueensgambit/java/testcases/checkmate_in_one.txt");
@@ -72,6 +111,35 @@ public class SolverTesting {
         catch(FileNotFoundException e){
             System.out.println("Could not find file! " + e.getLocalizedMessage());
             fail();
+        }
+    }
+
+    @Test void getRandomPuzzleFromTestcases(){
+
+    }
+
+    @Test void fetchRandomPuzzleFromLichess(){
+        try {
+            URI uri = new URI("https://lichess.org/api/puzzle/W8Af8");
+            HttpURLConnection conn = (HttpURLConnection) uri.toURL().openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine+"\n");
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println("Response from API:");
+            System.out.println(response.toString());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
